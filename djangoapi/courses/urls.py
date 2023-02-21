e2f62884f266451c595238cf4c2e62e8eead569c
juanfrancisco.calizgonzalez <juanfrancisco.calizgonzalez@devo.com>
2023-02-21 15:43:57 +0100

    Added some interactivity to the API

--- djangoapi/courses/urls.py
+++ djangoapi/courses/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseView)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
